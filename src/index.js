ace849527717d12576124c5f14347c88dc17fca2
samueldlh <samueldlh.dev@gmail.com>
2021-04-21 23:22:27 -0500

    mock data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
+const user = require('./data/data.json');
 const { port } = require('./config');
 
 const server = http.createServer((req, res) => {
-    console.log('hi :)');
+    if(req.url === '/api/users'){
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.write(JSON.stringify(user));
+        res.end();
+    }else{
+        res.end();
+    }
 });
 
 server.listen(port, () => console.log(`Server runing on port ${port}`))
